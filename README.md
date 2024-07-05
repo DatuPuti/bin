b613deca85128f0a20de830f04d667add9592a57
DatuPuti <tborland@thomasborland.ca>
2024-07-05 18:58:52 -0400

    edited newTerminalSetup.sh (mostly)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# bin
-# bin
+# https://github.com/DatuPuti/bin.git
+bash/zsh shell scripts for setting up new linux installs
+
+Some scripts will require editing
--- newTerminalSetup.sh
+++ newTerminalSetup.sh
@@ -1,6 +1,57 @@
+
+# you will have to create the bin directory and clone bin.git manually
+# setup new linux install
+# git clone https://github.com/DatuPuti/bin.git
+#
 # clone gitCmd 
+cd ~/bin
+git clone https://github.com/DatuPuti/gitCmd.git
+
+# install homebrew
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+
+# install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+# add neovim to path for instant use
+# if using the .dotfiles repository then the .zshrc will have the path set
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+# get desired nerd fonts - install manually
+cd ~/Downloads
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
+
+# install oh-my-zsh and power10k
 cd ~
-mkdir bin
-cd bin
-git clone home/tborland/bin
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# install colorls using ruby gem
+gem install colorls
+
+# install commandline tools 
+sudo apt install stow -y
+brew install thefuck
+sudo apt install zoxide -y
+sudo apt ripgrep -y
+sudo apt install tmux
+sudo apt bat -y
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+# clone the configs from github
+cd ~
+git clone https://github.com/DatuPuti/.dotfiles.git
+cd .dotfiles
+stow --target=/home/tborland .
+
+# last thing to do is source the .zshrc to get things going
+# make sure we are using zsh shell
+cd ~
+sudo apt install zsh -y
+chsh -s /bin/zsh
 
